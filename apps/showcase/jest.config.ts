5e65b5cdbbb4ac226a4b61d9f40add9f64670f13
louiiuol <louis.godlewski@gmail.com>
2024-09-05 17:41:46 +0200

    moved apps

--- apps/showcase/jest.config.ts
+++ apps/showcase/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'showcase',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/showcase',
+  coverageDirectory: '../../coverage/apps/showcase',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
